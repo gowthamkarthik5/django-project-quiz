6a830084f85d4b675312ae7a3bd673829e67e001
gowthamkarthik5 <mgkarthik5.gmail.com>
2020-06-29 16:46:49 +0530

    First commit

---
+++ courses/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+from .models import Course, Module
+
+from django import forms
+from django.forms.widgets import RadioSelect
+
+
+ModuleFormSet = inlineformset_factory(Course,
+                                      Module,
+                                      fields=['title', 'description'],
+                                      extra=2,
+                                      can_delete=True)
+
+
+class QuestionForm(forms.Form):
+    def __init__(self, question, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        choice_list = [x for x in question.get_answers_list()]
+        self.fields["answers"] = forms.ChoiceField(choices=choice_list, widget=RadioSelect)
+
+
---
+++ courses/models.py
@@ -0,0 +1,180 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
+from .fields import OrderField
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created',on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(User,
+                                      related_name='courses_enrolled',
+                                      blank=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name='modules',on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return '{}. {}'.format(self.order, self.title)
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents',on_delete=models.CASCADE)
+    order = OrderField(blank=True, for_fields=['module'])
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':('text',
+                                                                    'video',
+                                                                    'image',
+                                                                    'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        ordering = ['order']
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related',on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+    def render(self):
+        return render_to_string('courses/content/{}.html'.format(self._meta.model_name), {'item': self})
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
+'''class CSVUploadsAdmin(admin.ModelAdmin):
+    model = CSVUpload
+    list_display= ('title',)
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuizAdminForm(forms.ModelForm):
+    """
+        below is from
+        http://stackoverflow.com/questions/11657682/
+        django-admin-interface-using-horizontal-filter-with-
+        inline-manytomany-field
+    """
+
+    class Meta:
+        model = Quiz
+        exclude = []
+
+    questions = forms.ModelMultipleChoiceField(
+        queryset=Question.objects.all().select_subclasses(),
+        required=False,
+        label=_("Questions"),
+        widget=FilteredSelectMultiple(
+            verbose_name=_("Questions"),
+            is_stacked=False))
+
+    def __init__(self, *args, **kwargs):
+        super(QuizAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['questions'].initial = \
+                self.instance.question_set.all().select_subclasses()
+
+    def save(self, commit=True):
+        quiz = super(QuizAdminForm, self).save(commit=False)
+        quiz.save()
+        quiz.question_set.set(self.cleaned_data['questions'])
+        self.save_m2m()
+        return quiz
+
+
+class QuizAdmin(admin.ModelAdmin):
+    form = QuizAdminForm
+
+    list_display = ('title', 'category', )
+    list_filter = ('category',)
+    search_fields = ('description', 'category', )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('category', )
+
+
+class MCQuestionAdmin(admin.ModelAdmin):
+    list_display = ('content', 'category', )
+    list_filter = ('category',)
+    fields = ('content', 'category',
+              'figure', 'quiz', 'explanation', 'answer_order')
+
+    search_fields = ('content', 'explanation')
+    filter_horizontal = ('quiz',)
+
+    inlines = [AnswerInline]
+
+
+class ProgressAdmin(admin.ModelAdmin):
+    """
+    to do:
+            create a user section
+    """
+    search_fields = ('user', 'score', )
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(MCQQuestion, MCQuestionAdmin)
+admin.site.register(Progress, ProgressAdmin)
+admin.site.register(CSVUpload, CSVUploadsAdmin)
+'''
+
+
+
